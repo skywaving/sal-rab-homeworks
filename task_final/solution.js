30e8c21e9b3c1fc2cec9358c56f04300f71b00be
Sasha M <almitrokhina@gmail.com>
2021-06-06 22:57:08 +0300

    task final test correction

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,19 +36,19 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: '', goods: [], order: {}};
     
-    title = {};
-    count = {}; 
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.push({ title: goods[i].title, count: goods[i].count });
     }
 
-    data.order.address = address;
-    data.order.sum = sum;
+    data.order = {
+        address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+        sum   
+    }
 
     data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({ data });
 
     return jsonData;
 }
